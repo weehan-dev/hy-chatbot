98a6f9df889f89d945bbe4aa6e9a979eb06bfba8
changhoi <0417kch@naver.com>
2019-11-03 14:13:20 +0900

    feat(env): proxy setup

--- src/loaders/expressLoader.js
+++ src/loaders/expressLoader.js
@@ -5,7 +5,7 @@ import api from '../routers';
 console.log(configs.ENV);
 
 export default (app) => {
-  app.set('port', configs.ENV === 'production' ? configs.PORT : 3000);
+  app.set('port', configs.PORT);
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use('/api', api);
