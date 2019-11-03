a9bbe6b0fcdc0e8738c830c1a1289a1689c7d2d5
changhoi <0417kch@naver.com>
2019-11-03 09:49:04 +0900

    feat(eb): env setup

--- src/loaders/expressLoader.js
+++ src/loaders/expressLoader.js
@@ -2,8 +2,10 @@ import express from 'express';
 import configs from '../configs';
 import api from '../routers';
 
+console.log(configs.ENV);
+
 export default (app) => {
-  app.set('port', configs.ENV === 'PROD' ? configs.PORT : 3000);
+  app.set('port', configs.ENV === 'production' ? configs.PORT : 3000);
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use('/api', api);
