f9c457a2626694404fde263a0b44f52dd39b95f4
changhoi <0417kch@naver.com>
2019-11-03 09:00:49 +0900

    feat(deploy): pm2 script modified, health check endpoint

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,3 +1,18 @@
+import express from 'express';
 import api from './v1';
 
-export default api;
+const router = express.Router();
+
+router.post('/test', (req, res) => {
+  console.log(req);
+  res.json({
+    message: 'test'
+  });
+});
+
+router.get('/health', (req, res) => {
+  res.json({ message: 'server is on' });
+});
+
+router.use('/api', api);
+export default router;
--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -3,17 +3,6 @@ import facility from './facility';
 
 const router = express.Router();
 
-router.post('/test', (req, res) => {
-  console.log(req);
-  res.json({
-    message: 'test'
-  });
-});
-
-router.get('/health', (req, res) => {
-  res.json({ message: 'server is on' });
-});
-
 router.use('facility', facility);
 
 export default router;
