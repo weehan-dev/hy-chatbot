8ffce755a3804ff14ce014033872b73763a75e23
changhoi <0417kch@naver.com>
2019-11-03 15:09:55 +0900

    feat(project): project setup

--- src/loaders/expressLoader.js
+++ src/loaders/expressLoader.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import logger from 'morgan';
+import helmet from 'helmet';
 import configs from '../configs';
 import api from '../routers';
 
-console.log(configs.ENV);
-
 export default (app) => {
   app.set('port', configs.PORT);
+  app.use(helmet());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(logger('dev'));
   app.use('/api', api);
 };
--- src/routers/v1/facility.js
+++ src/routers/v1/facility.js
@@ -2,4 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 
+router.post('ATM', (req, res) => {
+  console.log(req);
+  res.json('test');
+});
+
 export default router;
