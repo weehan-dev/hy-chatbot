ed1a41019111c1908238e1a63f6d01f23b0784b8
Jung <jungeun.lak@gmail.com>
2019-11-09 21:18:20 +0900

    feat(weat): 날씨 라우터연결 #나중에 위치 옮겨야 함

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import api from './v1';
+import handleWeather from '../services/serProvideWeather';
 
 const router = express.Router();
 
@@ -12,6 +13,11 @@ router.post('/test', (req, res) => {
 
 router.get('/health', (req, res) => {
   res.json({ message: 'server is on' });
+});
+
+router.post('/weather', (req, res) => {
+  console.log(req);
+  res.json(handleWeather());
 });
 
 router.use('/api', api);
