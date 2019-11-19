20e8dc0a2ac0fcba58a35908680b2b23e381c98e
Jung <jungeun.lak@gmail.com>
2019-11-19 17:03:40 +0900

    feat(weather): 현재시간기준으로 기상청 정보 받아오기 (url마다 기준 api공개 시간이 달라서 수정 필요)

--- src/services/serProvideWeather.js
+++ src/services/serProvideWeather.js
@@ -3,30 +3,60 @@
 import axios from 'axios';
 
 
+function handleDate(){
+    let today= new Date();
+    let year= today.getFullYear();
+    let month=today.getMonth()+1;
+    let day=today.getDate();
+
+    let date=`${year}${month}${day}`
+    return date;
+};
+
+function handleTime(){
+    let today= new Date();
+
+    let hours=today.getHours();
+    let minutes=today.getMinutes();
+
+    if (minutes<45){
+        hours--;
+    };
+
+    return hours;
+}
+
+function handleSpaceTime(){
+    let today= new Date();
+
+    let hours=today.getHours();
+}
+
 async function getWeather() {
     console.log("기상청api로 정보를 받아옴");
 
 
     const servicekey = process.env.serviceKey;
-    let date = 20191117;
-    let time = "2200";
-    let spacetime = "0700";
+    let basedate = handleDate();
+    let basetime = `${handleTime()}00`;
+    let spacetime = "2300";
 
     //초단기 실황
-    let url1 = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib?ServiceKey=${servicekey}&base_date=${date}&base_time=${time}&nx=55&ny=127&pageNo=1&numOfRows=10&_type=json`;
+    let url1 = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib?ServiceKey=${servicekey}&base_date=${basedate}&base_time=${basetime}&nx=61&ny=127&pageNo=1&numOfRows=10&_type=json`;
     //초단기 예보
-    let url2 = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData?ServiceKey=${servicekey}&base_date=${date}&base_time=${time}&nx=55&ny=127&pageNo=1&numOfRows=10&_type=json`;
+    let url2 = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData?ServiceKey=${servicekey}&base_date=${basedate}&base_time=${basetime}&nx=61&ny=127&pageNo=1&numOfRows=10&_type=json`;
     //동네 예보
-    let url3 = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?ServiceKey=${servicekey}&base_date=${date}&base_time=${spacetime}&nx=55&ny=127&pageNo=3&numOfRows=10&_type=json`;
-      
-    let weatherData;
+    let url3 = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?ServiceKey=${servicekey}&base_date=${basedate}&base_time=${spacetime}&nx=61&ny=127&pageNo=3&numOfRows=10&_type=json`;
+
+    console.log(url1);
+
+    let weatherData={};
+
     await axios.get(url1).then(response => {
-        weatherData = response.data;
-        console.log("1", weatherData);
+        // weatherData=response.data.response
+        weatherData = response.data.response.body.items;
     });
     
-    console.log("2", weatherData);
-    console.log(3);
     return weatherData;
 }
 
@@ -54,12 +84,12 @@ function handleWeather() {
     return weather;
 }
 
-function Weather() {
+async function Weather() {
 
-    let result = handleWeather();
-    console.log("***");
+    let result = await getWeather();
+    console.log("result");
 
     return result;
 };
 
-export default getWeather;
+export default Weather;
