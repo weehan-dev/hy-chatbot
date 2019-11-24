86208c3b8c6309970bcc66ec01beb87814853f1b
Jung <jungeun.lak@gmail.com>
2019-11-24 15:39:07 +0900

    feat(weather): 학교생활 관련 이름 생각하다가 livingPoint로 다시 변경함

--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import facility from './facility';
-import school from './schoolLife';
+import livingPoint from './schoolLife';
 
 const router = express.Router();
 
 router.use('facility', facility);
-router.use('/school', school);
+router.use('/livingPoint', livingPoint);
 
 export default router;
