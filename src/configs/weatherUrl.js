ba2280e19783beb2d94fb7b5c72728d4ee205ae5
Jung <jungeun.lak@gmail.com>
2019-11-25 01:37:03 +0900

    feat(weather): moment-timezone으로 서버위치랑 상관 없이 한국시간 기준으로 돌아가게 수정.

--- src/configs/weatherUrl.js
+++ src/configs/weatherUrl.js
@@ -1,15 +1,23 @@
-function spaceDate() {
-  const today = new Date();
+import moment from 'moment-timezone';
 
-  if (today.getHours() < 3) {
-    today.setDate(today.getDate() - 1);
+
+// 요청받은 시각의 서울 시각
+function seoulTime() {
+  const date = moment().tz('Asia/Seoul');
+
+  return date;
+}
+
+function spaceDate() {
+  let today = seoulTime();
+
+  if (today.format('HH') < 3) {
+    today = today.add('-1', 'd');
   }
 
-  const year = today.getFullYear();
-  const month = (`0${today.getMonth() + 1}`).slice(-2);
-  //   const month = ('0' + (today.getMonth() + 1)).slice(-2);
-
-  const day = (`0${today.getDate()}`).slice(-2);
+  const year = today.format('YYYY');
+  const month = today.format('MM');
+  const day = today.format('DD');
 
   const date = `${year}${month}${day}`;
 
@@ -17,10 +25,10 @@ function spaceDate() {
 }
 
 function spaceTime() {
-  const today = new Date();
+  const today = seoulTime();
   let hours = '0200';
 
-  if (today.getHours() < 3) {
+  if (today.format('HH') < 3) {
     hours = '2300';
   }
 
@@ -29,35 +37,55 @@ function spaceTime() {
 
 // 기상청 api에 넣을 시간 파라미터 반환 함수
 function handleTime() {
-  const today = new Date();
-  const minutes = today.getMinutes();
-
+  let today = seoulTime();
+  const minutes = today.format('mm');
   if (minutes < 45) {
-    today.setHours(today.getHours() - 1);
+    today = today.add('-1', 'h');
   }
 
-  const hours = (`0${today.getHours()}`).slice(-2);
+  const hours = today.format('HH00');
 
   return hours;
 }
 
 // 기상청 api에 넣을 날짜 파라미터 반환 함수
 function handleDate() {
-  const today = new Date();
-
-  const minutes = today.getMinutes();
+  let today = seoulTime();
+  const minutes = today.format('mm');
 
   if (minutes < 45) {
-    today.setHours(today.getHours() - 1);
+    today = today.add('-1', 'h');
   }
 
-  const year = today.getFullYear();
-  const month = (`0${today.getMonth() + 1}`).slice(-2);
-  const day = (`0${today.getDate()}`).slice(-2);
+  const year = today.format('YYYY');
+  const month = today.format('MM');
+  const day = today.format('DD');
 
   const date = `${year}${month}${day}`;
+
   return date;
 }
+
+
+export function getShortTermStatus(serviceKey) {
+  const basedate = handleDate();
+  const basetime = handleTime();
+
+  // 초단기 실황
+  const shortTermStatusUrl = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib?ServiceKey=${serviceKey}&base_date=${basedate}&base_time=${basetime}&nx=61&ny=127&pageNo=1&numOfRows=10&_type=json`;
+  return shortTermStatusUrl;
+}
+
+
+export function getShortTermForecast(serviceKey) {
+  const basedate = handleDate();
+  const basetime = handleTime();
+
+  // 초단기 예보
+  const shortTermForecastUrl = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData?ServiceKey=${serviceKey}&base_date=${basedate}&base_time=${basetime}&nx=61&ny=127&pageNo=1&numOfRows=25&_type=json`;
+  return shortTermForecastUrl;
+}
+
 
 export function getRegionForecast(serviceKey) {
   const spacedate = spaceDate();
@@ -65,24 +93,5 @@ export function getRegionForecast(serviceKey) {
 
   // 동네 예보
   const regionForecastUrl = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?ServiceKey=${serviceKey}&base_date=${spacedate}&base_time=${spacetime}&nx=61&ny=127&pageNo=1&numOfRows=175&_type=json`;
-
   return regionForecastUrl;
-}
-
-export function getShortTermForecast(serviceKey) {
-  const basedate = handleDate();
-  const basetime = `${handleTime()}00`;
-
-  // 초단기 예보
-  const shortTermForecastUrl = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData?ServiceKey=${serviceKey}&base_date=${basedate}&base_time=${basetime}&nx=61&ny=127&pageNo=1&numOfRows=25&_type=json`;
-  return shortTermForecastUrl;
-}
-
-export function getShortTermStatus(serviceKey) {
-  const basedate = handleDate();
-  const basetime = `${handleTime()}00`;
-
-  // 초단기 실황
-  const shortTermStatusUrl = `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib?ServiceKey=${serviceKey}&base_date=${basedate}&base_time=${basetime}&nx=61&ny=127&pageNo=1&numOfRows=10&_type=json`;
-  return shortTermStatusUrl;
 }
