4d7e31a7f0087affa4836937bf750e9378a91c1e
Jung <jungeun.lak@gmail.com>
2020-07-26 18:37:41 +0900

    fix(weather): 기상청 제공 API변경에 따른 데이터 요청 url 변경

--- src/configs/index.js
+++ src/configs/index.js
@@ -16,9 +16,9 @@ export default {
   URL: {
     LIBRARY: 'https://lib.hanyang.ac.kr/smufu-api/pc/1/rooms-at-seat',
     DIET: 'http://app.ucan.or.kr/api/diet/hanyang/',
-    SHORTTERM_STATUS: `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib?ServiceKey=${process.env.WEATHER_SERVICE_KEY}&nx=61&ny=127&pageNo=1&numOfRows=10&_type=json`,
-    SHORTTERM_FORECAST: `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData?ServiceKey=${process.env.WEATHER_SERVICE_KEY}&nx=61&ny=127&pageNo=1&numOfRows=25&_type=json`,
-    SPACE_FORECAST: `http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?ServiceKey=${process.env.WEATHER_SERVICE_KEY}&nx=61&ny=127&pageNo=1&numOfRows=175&_type=json`,
+    SHORTTERM_STATUS: `http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst?serviceKey=${process.env.WEATHER_SERVICE_KEY}&numOfRows=10&pageNo=1&nx=61&ny=127&dataType=json`,
+    SHORTTERM_FORECAST: `http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtFcst?serviceKey=${process.env.WEATHER_SERVICE_KEY}&numOfRows=25&pageNo=1&nx=61&ny=127&dataType=json`,
+    SPACE_FORECAST: `http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=${process.env.WEATHER_SERVICE_KEY}&numOfRows=175&pageNo=1&nx=61&ny=127&dataType=json`,
     SUNNY_IMAGE: 'https://user-images.githubusercontent.com/52434876/71237703-d026a600-2349-11ea-8e79-14f0aed22118.jpg',
     CLOUDY_IMAGE: 'https://user-images.githubusercontent.com/52434876/71237699-cf8e0f80-2349-11ea-8bf0-4d21f791c822.jpg',
     RAINY_IMAGE: 'https://user-images.githubusercontent.com/52434876/71237700-d026a600-2349-11ea-9bd0-975f46f5d8ff.jpg',
